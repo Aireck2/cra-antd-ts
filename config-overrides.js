ecd4aafdcf63233198808094c2c96375de76583f
@erick <erick.hx22@gmail.com>
2020-05-03 11:46:07 -0500

    feat: include ts files to tsconfig  (#3)

--- config-overrides.js
+++ config-overrides.js
@@ -1,28 +1,42 @@
 const {
   override,
   fixBabelImports,
-  addLessLoader
-} = require('customize-cra');
+  disableEsLint,
+  addLessLoader,
+  useBabelRc,
+} = require("customize-cra");
 
-const path = require('path');
-const lessToJS = require('less-vars-to-js');
-const fs = require('fs');
+const path = require("path");
+const lessToJS = require("less-vars-to-js");
+const fs = require("fs");
 
 // Where your antd-custom.less file lives
-const themeVariables = lessToJS(fs.readFileSync(path.resolve(__dirname, 'src/assets/antd-custom.less'), 'utf8'));
+const themeVariables = lessToJS(
+  fs.readFileSync(
+    path.resolve(__dirname, "src/assets/antd-custom.less"),
+    "utf8"
+  )
+);
+
 module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
+  disableEsLint(),
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
     style: true,
   }),
+  fixBabelImports("formik-antd", {
+    libraryName: "formik-antd",
+    libraryDirectory: "es",
+    style: true,
+  }),
+  useBabelRc(),
   addLessLoader({
     javascriptEnabled: true,
     modifyVars: themeVariables,
   })
 );
 
-// eslint-disable-next-line no-unused-vars
 const config = {
   webpack(config) {
     config.resolve.modules.push(__dirname);
